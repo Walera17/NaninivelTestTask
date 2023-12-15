83d498f78652c95eb4dd007ad390cba49d673e21
DESKTOP-K3JDAAT\Walera <aluschta17@gmail.com>
2023-12-15 07:39:02 +0200

    05. Create Interact

--- Assets/Naninovel/Runtime/Game/GameCommand.cs
+++ Assets/Naninovel/Runtime/Game/GameCommand.cs
@@ -1,3 +1,6 @@
+using Naninovel.UI;
+using UnityEngine;
+
 namespace Naninovel.Runtime.Game
 {
     [CommandAlias("startGame")]
@@ -23,6 +26,32 @@ namespace Naninovel.Runtime.Game
         {
             GameService gameService = Engine.GetService<GameService>();
             gameService.label = Name.Value;
+            return default;
+        }
+    }
+
+    [CommandAlias("disableBlockRayCast")]
+    public class DisableBlockRayCast : Command
+    {
+        public StringParameter Name;
+
+        public override UniTask ExecuteAsync(AsyncToken asyncToken = default)
+        {
+            UIManager uiManager = Engine.GetService<UIManager>();
+            uiManager.GetUI<ContinueInputUI>().GetComponent<CanvasGroup>().blocksRaycasts = false;
+            return default;
+        }
+    }
+
+    [CommandAlias("enableBlockRayCast")]
+    public class EnableBlockRayCast : Command
+    {
+        public StringParameter Name;
+
+        public override UniTask ExecuteAsync(AsyncToken asyncToken = default)
+        {
+            UIManager uiManager = Engine.GetService<UIManager>();
+            uiManager.GetUI<ContinueInputUI>().GetComponent<CanvasGroup>().blocksRaycasts = true;
             return default;
         }
     }
