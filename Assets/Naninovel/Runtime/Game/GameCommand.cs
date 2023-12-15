af7fdb9eb83bd9b8f00e5255d902d6b89b4f9c27
DESKTOP-K3JDAAT\Walera <aluschta17@gmail.com>
2023-12-16 00:12:54 +0200

    08. Delete ContinueInputUI. Refactoring

--- Assets/Naninovel/Runtime/Game/GameCommand.cs
+++ Assets/Naninovel/Runtime/Game/GameCommand.cs
@@ -1,6 +1,3 @@
-using Naninovel.UI;
-using UnityEngine;
-
 namespace Naninovel.Runtime.Game
 {
     [CommandAlias("startGame")]
@@ -26,32 +23,6 @@ namespace Naninovel.Runtime.Game
         {
             GameService gameService = Engine.GetService<GameService>();
             gameService.label = Name.Value;
-            return default;
-        }
-    }
-
-    [CommandAlias("disableBlockRayCast")]
-    public class DisableBlockRayCast : Command
-    {
-        public StringParameter Name;
-
-        public override UniTask ExecuteAsync(AsyncToken asyncToken = default)
-        {
-            UIManager uiManager = Engine.GetService<UIManager>();
-            uiManager.GetUI<ContinueInputUI>().GetComponent<CanvasGroup>().blocksRaycasts = false;
-            return default;
-        }
-    }
-
-    [CommandAlias("enableBlockRayCast")]
-    public class EnableBlockRayCast : Command
-    {
-        public StringParameter Name;
-
-        public override UniTask ExecuteAsync(AsyncToken asyncToken = default)
-        {
-            UIManager uiManager = Engine.GetService<UIManager>();
-            uiManager.GetUI<ContinueInputUI>().GetComponent<CanvasGroup>().blocksRaycasts = true;
             return default;
         }
     }
